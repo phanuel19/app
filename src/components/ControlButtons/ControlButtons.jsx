0a58c26fa2162dab8ebb0e10af85ea9ae9e325b3
phanuel19 <dagbo@gmail.com>
2024-12-27 13:53:24 +0000

    fin du projet

--- src/components/ControlButtons/ControlButtons.jsx
+++ src/components/ControlButtons/ControlButtons.jsx
@@ -10,7 +10,7 @@ function ControlButtons() {
             <h2><GrAction />Actions</h2>
             <div className="controlZone">
                 <div className="buttons">
-                    <img src={powerOn} alt="" />
+                    <img src={powerOn} alt="" className="img-btn"/>
                     <h3>Activation du système</h3>
                     <p className="description">
                         Activez ou désactivez le dispositif principal pour démarrer ou arrêter l&apos;ensemble des systèmes connectés. Assurez-vous que tout est opérationnel avant de poursuivre.
@@ -20,7 +20,7 @@ function ControlButtons() {
 
              
                 <div className="buttons">
-                    <img src={check} alt="" />
+                    <img src={check} alt=""className="img-btn"/>
                     <h3>Surveillance des données</h3>
                     <p className="description">
                         Consultez les données en temps réel pour surveiller l&apos;état de vos cultures, le sol et les conditions climatiques. Prenez des décisions éclairées grâce à des analyses détaillées.
@@ -30,7 +30,7 @@ function ControlButtons() {
 
 
                 <div className="buttons">
-                    <img src={watering} alt="" />
+                    <img src={watering} alt=""className="img-btn" />
                     <h3>Gestion de l&apos;irrigation</h3>
                     <p className="description">
                         Gérez l&apos;irrigation de vos cultures grâce à notre système intelligent. Optimisez l&apos;apport en eau selon les besoins détectés pour économiser les ressources et maximiser la croissance.
@@ -40,7 +40,7 @@ function ControlButtons() {
 
              
                 <div className="buttons">
-                    <img src={harvest} alt="" />
+                    <img src={harvest} alt="" className="img-btn"/>
                     <h3>Planification des cultures</h3>
                     <p className="description">
                         Planifiez vos cycles agricoles en fonction des données de votre terrain et des prévisions climatiques. Obtenez des recommandations pour choisir les meilleures périodes de semis et de récolte.
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,19 @@
+import { Container, Row, Col } from "react-bootstrap";
 function Footer() {
 
+    let date = new Date();
+    let year = date.getFullYear();
     return (
-        <footer>
-           <div className="up">Footer- up </div>
-           <div className="center"> Footer center</div>
-           <div className="down"> Footer-down</div>
-        </footer>
+          <Container fluid className="footer">
+            <Row>
+              <Col md="4" className="footer-copywright">
+                <span> Projet App 3 - Equipe NOVA</span>
+              </Col>
+              <Col md="4" className="footer-copywright">
+                <span>Copyright © {year}</span>
+              </Col>
+            </Row>
+          </Container>
     )
 }
 
